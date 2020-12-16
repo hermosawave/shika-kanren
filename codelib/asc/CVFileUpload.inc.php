a36588d7cb7596d37fd141d8195c443c167f8d9f
Jet Villegas <jet@junglecode.net>
2020-12-16 22:56:13 +0900

    Update CVFileUpload.inc.php
    Update headers for debugging

--- codelib/asc/CVFileUpload.inc.php
+++ codelib/asc/CVFileUpload.inc.php
@@ -503,14 +503,13 @@ class CVFileUpload extends CVText
 
 		//-- send out all headers
 		header( "HTTP/1.1 200 OK" );
-		header( "Expires: Mon, 26 Jul 1997 05:00:00 GMT" );
+		header( "Expires: Mon, 26 Jul 1998 05:00:00 GMT" );
 		header( "Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT" );
 		header( "cache-Control: no-store, no-cache, must-revalidate" );
 		header( "cache-Control: post-check=0, pre-check=0", false );
 		header( "Pragma: no-cache" );
 		header( "Content-Type: image/jpg" );
 		header( "Content-Length: $Size" );
-		//-- and finally the image itself
 		readfile( $path );
 		exit;
 	}
